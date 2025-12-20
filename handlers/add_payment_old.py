913fe822b3d43ddd738d9a54b679a1d38d71570a
MadGuest <kregdes@gmail.com>
2025-12-20 19:23:56 +0300

    feat: adding payment process goes in a single message

--- handlers/add_payment_old.py
+++ handlers/add_payment_old.py
@@ -4,7 +4,6 @@ from aiogram.filters import Command
 from aiogram.fsm.context import FSMContext
 from aiogram.fsm.state import State, StatesGroup
 from aiogram.utils.chat_action import ChatActionSender
-from utils.single_message_dialog import SingleMessageDialog
 
 
 # Состояния для добавления платежа
@@ -13,49 +12,54 @@ class Payment(StatesGroup):
     category = State()
     amount = State()
     due_date = State()
-    finish = State()
 
 
 add_payment_router = Router()
 
-smd = SingleMessageDialog()
 
 @add_payment_router.message(Command("add_payment"))
 async def add_payment_handler(message: types.Message, state: FSMContext):
-    await smd.initial_message(message, state, text="Название:")
+    async with ChatActionSender.typing(bot=message.bot, chat_id=message.chat.id):
+        await asyncio.sleep(2)
+        await message.answer("Название платежа:")
     await state.set_state(Payment.name)
 
-@add_payment_router.message(Payment.name)
+
+@add_payment_router.message(F.text, Payment.name)
 async def capture_payment_name(message: types.Message, state: FSMContext):
     await state.update_data(name=message.text)
-    await smd.next_step(message, state, text="Категория:")
-    await state.set_state(Payment.category)   
+    async with ChatActionSender.typing(bot=message.bot, chat_id=message.chat.id):
+        await asyncio.sleep(2)
+        await message.answer("Категория платежа:")
+    await state.set_state(Payment.category)
 
-@add_payment_router.message(Payment.category)
+
+@add_payment_router.message(F.text, Payment.category)
 async def capture_payment_category(message: types.Message, state: FSMContext):
     await state.update_data(category=message.text)
-    await smd.next_step(message, state, text="Сумма:")
-    await state.set_state(Payment.amount)   
+    async with ChatActionSender.typing(bot=message.bot, chat_id=message.chat.id):
+        await asyncio.sleep(2)
+        await message.answer("Сумма:")
+    await state.set_state(Payment.amount)
 
 
-@add_payment_router.message(Payment.amount)
+@add_payment_router.message(F.text, Payment.amount)
 async def capture_payment_amount(message: types.Message, state: FSMContext):
     await state.update_data(amount=message.text)
-    await smd.next_step(message, state, text="Дата платежа:")
+    async with ChatActionSender.typing(bot=message.bot, chat_id=message.chat.id):
+        await asyncio.sleep(2)
+        await message.answer("Дата платежа:")
     await state.set_state(Payment.due_date)
 
 
-@add_payment_router.message(Payment.due_date)
+@add_payment_router.message(F.text, Payment.due_date)
 async def capture_payment_due_date(message: types.Message, state: FSMContext):
     await state.update_data(due_date=message.text)
     data = await state.get_data()
-    answer = (f"Добавлен платеж:\n"
+    await message.answer(f"Добавлен платеж:\n"
                          f"{data.get('name')}\n"
                          f"{data.get('category')}\n"
                          f"{data.get('amount')}\n"
                          f"{data.get('due_date')}"
                          )
-    await smd.last_step(message, state, answer)
-    await asyncio.sleep(0.1)
-    await state.set_state(None)
-    
+    await state.clear()
---
+++ utils/single_message_dialog.py
@@ -0,0 +1,92 @@
+import asyncio
+from aiogram import Bot
+from aiogram.types import Message, CallbackQuery, InlineKeyboardMarkup
+from aiogram.fsm.context import FSMContext
+
+class SingleMessageDialog:
+    '''
+    Организует диалог с юзером в одном единственном сообщении    
+    '''
+    
+    @staticmethod
+    async def initial_message(message: Message,
+                              state: FSMContext,
+                              text: str="",
+                              reply_markup: InlineKeyboardMarkup | None=None
+                              ) -> None:
+        bot_message = await message.answer(text, reply_markup=reply_markup)
+        await state.update_data(
+            prev_message_id = bot_message.message_id,
+            dialog_bot=message.bot)
+
+    @staticmethod
+    async def next_step(update: Message|CallbackQuery,
+                        state: FSMContext,
+                        text: str,
+                        reply_markup: InlineKeyboardMarkup | None=None
+                        ) -> None:
+        data = await state.get_data()
+        dialog_bot = data.get("dialog_bot")
+        prev_message_id = data.get("prev_message_id")
+
+        if isinstance(update, Message) and update.text:
+            try:
+                await update.delete()
+            except:
+                pass
+
+        if prev_message_id and update.chat and update.chat.id:
+            try:
+                await dialog_bot.edit_message_text(
+                    text=text,
+                    chat_id=update.chat.id,
+                    message_id=prev_message_id,
+                    reply_markup=reply_markup
+                )
+            except:
+                bot_message = await update.message.reply(text,
+                                                         reply_markup=reply_markup
+                                                         )
+                await state.update_data(prev_message_id=bot_message.message_id)
+
+    @staticmethod
+    async def last_step(update: Message|CallbackQuery,
+                        state: FSMContext,
+                        text: str,
+                        reply_markup: InlineKeyboardMarkup | None=None
+                        ) -> None:
+        data = await state.get_data()
+        dialog_bot = data.get("dialog_bot")
+        prev_message_id = data.get("prev_message_id")
+
+        if isinstance(update, Message):
+            try:
+                await update.delete()
+            except:
+                pass
+
+        chat_id = update.chat.id
+
+        if prev_message_id:
+            try:
+                await dialog_bot.edit_message_text(
+                    text=text, 
+                    chat_id=chat_id,
+                    message_id=prev_message_id,
+                    reply_markup=reply_markup
+                )
+                return
+            except:
+                try:
+                    await dialog_bot.delete_message(chat_id, prev_message_id)
+                except:
+                    pass
+
+            await dialog_bot.send_message(chat_id, text, reply_markup=reply_markup)
+    
+
+
+
+
+        
+
