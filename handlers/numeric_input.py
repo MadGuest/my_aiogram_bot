1029971360989e0ec9c599ff8a1ded234b3036f2
MadGuest <kregdes@gmail.com>
2026-01-08 19:46:24 +0300

    Added stub callback handlers for action buttons

--- handlers/numeric_input.py
+++ handlers/numeric_input.py
@@ -1,11 +1,11 @@
 from aiogram import Router, F
 from aiogram.filters import StateFilter
 from aiogram.fsm.state import State
-from aiogram.types import Message
+from aiogram.types import Message, CallbackQuery
 from decimal import Decimal
 # from filters.numeric_input_filter import PositiveNumberFilter, NegativeNumberFilter
 from filters.numeric_input_filter import NumberFilter
-from keyboards.inline import get_actions_kb
+from keyboards.inline import get_actions_kb, ActionsCallbackFactory
 router = Router()
 
 @router.message(F.text, StateFilter(None),NumberFilter())
@@ -13,6 +13,27 @@ async def numeric_input_handler(message: Message, number: Decimal):
     await message.delete()
     await message.answer("Добавить операцию", reply_markup=get_actions_kb())
 
+
+@router.callback_query(ActionsCallbackFactory.filter(F.action == "income"))
+async def add_income(callback: CallbackQuery):
+    
+    await callback.message.edit_text("Обработчик для добавления прихода")
+    await callback.answer()
+
+@router.callback_query(ActionsCallbackFactory.filter(F.action == "outcome"))
+async def add_outcome(callback: CallbackQuery):
+    await callback.message.edit_text("Обработчик для добавления расхода")
+    await callback.answer()
+
+@router.callback_query(ActionsCallbackFactory.filter(F.action == "transfer"))
+async def add_transfer(callback: CallbackQuery):
+    await callback.message.edit_text("Обработчик для добавления перевода")
+    await callback.answer()
+
+@router.callback_query(ActionsCallbackFactory.filter(F.action == "payment"))
+async def menu_payment(callback: CallbackQuery):
+    await callback.message.edit_text("Обработчик для добавления платежа")
+    await callback.answer()
 
 
 
